9032a1acc18d4287953e9e0009845421adcf2cbd
p6steve <40125330+p6steve@users.noreply.github.com>
2022-12-27 17:07:00 +0000

    Create Build.pm

---
+++ Build.pm
@@ -0,0 +1,28 @@
+class Build {
+    method build($dist-path) {
+        
+        chdir $*HOME;
+        mkdir '.racl-config';
+        chdir '.racl-config';
+        
+my $contents = q:to/END/
+instance:
+    image: 'ami-0f540e9f488cfa27d'
+    type: 't2.micro'
+    security-group:
+        name: 'MySG'
+        rules:
+            - inbound:
+                port: 80
+                cidr: '0.0.0.0/0'
+            - inbound:
+                port: 443 
+                cidr: '0.0.0.0/0'
+END
+        qqx`cat $contents > aws-ec2-launch.yaml`;
+        
+        warn 'Build successful';
+        
+        exit 0
+    }
+}
